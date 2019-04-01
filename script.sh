91fd2867c71cdfeeed26aa00a225717dfb6318b3
liciascl <40764768+liciascl@users.noreply.github.com>
2019-04-01 16:16:13 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,4 @@ git push
 sudo apt-get update
 sudo apt autoclean
 echo "finish update"
-rm -rf /home/pi/net/control.py
-mv ~/turtlebot3/Menu_Troca_Rede/control.py /home/pi/net
-rm -rf /home/pi/turtlebot3/Bumper
-rm -rf /home/pi/turtlebot3/Menu_Troca_Rede
-rm -rf /home/pi/turtlebot3/Suportes_Turtlebot3
+
