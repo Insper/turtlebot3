7a2c59148fe46c16be249f428d2cff5fce8d7fae
Licia Sales <40764768+liciascl@users.noreply.github.com>
2019-04-22 10:44:32 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,16 @@ git checkout master
 git push
 sudo apt-get update
 sudo apt autoclean
-echo "finish update"
 
+if [ -f ~/update.txt ]
+then
+    echo "Ja foi atualizado..."
+else
+    touch ~/update.txt
+    echo "Atualizando o ajuste da camera..." > ~/update.txt
+    date >> ~/update.txt
+    sed -i '7i\    <param name="hFlip" type="int" value="1" />\' ~/catkin_ws/src/turtlebot3/turtlebot3_bringup/launch/turtlebot3_rpicamera.launch
+    sed -i '8i\    <param name="vFlip" type="int" value="1" />\' ~/catkin_ws/src/turtlebot3/turtlebot3_bringup/launch/turtlebot3_rpicamera.launch 
+fi
+
+echo "finish update"
